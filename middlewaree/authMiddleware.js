4c596bf1afeb1ba5eefddbc32acf917da2ad0c52
Faor <ivan.dovgoniuk@gmail.com>
2022-10-01 12:21:14 +0300

    commit

--- middlewaree/authMiddleware.js
+++ middlewaree/authMiddleware.js
@@ -1,7 +1,5 @@
 const jwt = require('jsonwebtoken')
 
-const clientURL = process.env.NODE_ENV === 'development' ? process.env.DEV_CLIENT_URL : process.env.PRO_CLIENT_URL
-
 module.exports = function (req, res, next) {
     if (req.method === "OPTIONS") {
         next()
@@ -10,13 +8,15 @@ module.exports = function (req, res, next) {
         const secret = process.env.JWT_ACCESS_SECRET;
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
-            return res.redirect(`${clientURL}/login`)
+            return res.status(403)
+                      .json({message: "User is not authorized"})
 
         }
         const userID = jwt.verify(token, secret)
         req.userData = userID
         next()
     } catch (e) {
-        return res.redirect(`${clientURL}/login`)
+        return res.status(403)
+                  .json({message: "User is not authorized"})
     }
 };
